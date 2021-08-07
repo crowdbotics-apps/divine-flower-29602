3d06aa2b8ade9bac2d719f415509a0d9781f9bcd
Crowdbotics <team@crowdbotics.com>
2021-08-07 12:21:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "divine_flower_29602Identifier",
+  url: "https://divine_flower_29602Identifier.botics.co",
+  api: "https://divine_flower_29602Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
